a27196a4bf8acc4abc8452da6694bd039c54fe04
87yh97 <yakubets.ns@edu.spbstu.ru>
2020-12-06 18:10:45 +0300

    7 lesson

--- src/lesson7/JavaDynamicTasks.java
+++ src/lesson7/JavaDynamicTasks.java
@@ -2,6 +2,7 @@ package lesson7;
 
 import kotlin.NotImplementedError;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("unused")
@@ -9,7 +10,7 @@ public class JavaDynamicTasks {
     /**
      * Наибольшая общая подпоследовательность.
      * Средняя
-     *
+     * <p>
      * Дано две строки, например "nematode knowledge" и "empty bottle".
      * Найти их самую длинную общую подпоследовательность -- в примере это "emt ole".
      * Подпоследовательность отличается от подстроки тем, что её символы не обязаны идти подряд
@@ -18,14 +19,55 @@ public class JavaDynamicTasks {
      * Если есть несколько самых длинных общих подпоследовательностей, вернуть любую из них.
      * При сравнении подстрок, регистр символов *имеет* значение.
      */
+    //Трудоемкость: O(m*n) где m, n - длины строк
+    //Ресурсоемкость: O(m*n)
     public static String longestCommonSubSequence(String first, String second) {
-        throw new NotImplementedError();
+        if (first.equals("") || second.equals("")) return "";
+
+        int[][] commonChars = new int[first.length() + 1][second.length() + 1];
+
+        //for (int i = 0; i < first.length(); i++ )
+
+        for (int i = 1; i <= first.length(); i++) {
+            for (int j = 1; j <= second.length(); j++) {
+                if (first.charAt(i - 1) == second.charAt(j - 1)) {
+                    commonChars[i][j] = commonChars[i - 1][j - 1] + 1;
+                } else {
+                    commonChars[i][j] = Math.max(commonChars[i - 1][j], commonChars[i][j - 1]);
+                }
+            }
+        }
+
+
+        int i = first.length();
+        int j = second.length();
+        StringBuilder commonSequence = new StringBuilder();
+        while (j > 0 && i > 0) {
+            if (first.charAt(i - 1) == second.charAt(j - 1)) {
+                commonSequence.append(first.charAt(i - 1));
+                i--;
+                j--;
+            } else {
+                if (commonChars[i - 1][j] >= commonChars[i][j - 1]) i--;
+                else j--;
+            }
+        }
+//        for (int k = 1; k <= first.length(); k++) {
+//            for (int m = 1; m <= second.length(); m++) {
+//                System.out.print(commonChars[k][m] + ", ");
+//            }
+//            System.out.println();
+//        }
+        /*commonSequence = */
+        commonSequence.reverse();
+        System.out.println("CommonSequence " + commonSequence.toString());
+        return commonSequence.toString();
     }
 
     /**
      * Наибольшая возрастающая подпоследовательность
      * Сложная
-     *
+     * <p>
      * Дан список целых чисел, например, [2 8 5 9 12 6].
      * Найти в нём самую длинную возрастающую подпоследовательность.
      * Элементы подпоследовательности не обязаны идти подряд,
@@ -35,27 +77,92 @@ public class JavaDynamicTasks {
      * В примере ответами являются 2, 8, 9, 12 или 2, 5, 9, 12 -- выбираем первую из них.
      */
     public static List<Integer> longestIncreasingSubSequence(List<Integer> list) {
-        throw new NotImplementedError();
+        ArrayList<ArrayList<Integer>> sequenceList = new ArrayList<>();
+
+        if (list.size() > 0) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            temp.add(list.get(0));
+            sequenceList.add(temp);
+        } else {
+            return new ArrayList<>();
+        }
+
+        for (Integer newNum : list) {
+            nextSequence: for (int i = 0; i < sequenceList.size(); i++) {
+                ArrayList<Integer> subList = sequenceList.get(i);
+                if (newNum > subList.get(subList.size() - 1)) {
+                    ArrayList<Integer> temp = new ArrayList<>(subList);
+                    temp.add(newNum);
+                    sequenceList.add(temp);
+                } else if (newNum < subList.get(subList.size() - 1)) {
+                    if (subList.size() > 1) {
+                        //subList.set(subList.size() - 1, newNum);
+                        ArrayList<Integer> temp = new ArrayList<>(subList);
+                        temp.set(subList.size() - 1, newNum);
+                        sequenceList.add(temp);
+                    } else {
+                        ArrayList<Integer> temp = new ArrayList<>();
+                        temp.add(newNum);
+                        sequenceList.add(temp);
+                    }
+                }
+
+                for (Integer subNum : subList) {
+                    if (newNum < subNum) {
+                        for (ArrayList<Integer> firstInstance : sequenceList) {
+                            if (firstInstance.size() == (subList.indexOf(subNum) + 1) &&
+                                    firstInstance.get(firstInstance.size() - 1).equals(newNum)) break nextSequence;
+                        }
+                        ArrayList<Integer> temp = new ArrayList<>(subList.subList(0, subList.indexOf(subNum)));
+                        temp.add(newNum);
+                        sequenceList.add(temp);
+                    }
+                }
+            }
+        }
+
+        //for ()
+
+        int maxSize = 0;
+        ArrayList<Integer> longestSequence = new ArrayList<>();
+        for (ArrayList<Integer> subList : sequenceList) {
+            System.out.println(subList);
+            if (subList.size() > maxSize) {
+                longestSequence = subList;
+                maxSize = subList.size();
+            } else if (subList.size() == maxSize) {
+                for (int i = 0; i < maxSize; i++) {
+                    if (list.indexOf(subList.get(i)) < list.indexOf(longestSequence.get(i))) longestSequence = subList;
+                    else if (list.indexOf(subList.get(i)) > list.indexOf(longestSequence.get(i))) break;
+                }
+            }
+        }
+        System.out.println("=================================================");
+        return longestSequence;
     }
+
+//    public static List<Integer> atomicSubSequence(List<Integer> list) {
+//
+//    }
 
     /**
      * Самый короткий маршрут на прямоугольном поле.
      * Средняя
-     *
+     * <p>
      * В файле с именем inputName задано прямоугольное поле:
-     *
+     * <p>
      * 0 2 3 2 4 1
      * 1 5 3 4 6 2
      * 2 6 2 5 1 3
      * 1 4 3 2 6 2
      * 4 2 3 1 5 0
-     *
+     * <p>
      * Можно совершать шаги длиной в одну клетку вправо, вниз или по диагонали вправо-вниз.
      * В каждой клетке записано некоторое натуральное число или нуль.
      * Необходимо попасть из верхней левой клетки в правую нижнюю.
      * Вес маршрута вычисляется как сумма чисел со всех посещенных клеток.
      * Необходимо найти маршрут с минимальным весом и вернуть этот минимальный вес.
-     *
+     * <p>
      * Здесь ответ 2 + 3 + 4 + 1 + 2 = 12
      */
     public static int shortestPathOnField(String inputName) {
